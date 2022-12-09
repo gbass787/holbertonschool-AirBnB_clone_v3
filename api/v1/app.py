da49b3125c52d15860ae31111c4cb0d6c5c02a44
gbass787 <4551@holbertonschool.com>
2022-12-09 05:54:18 +0000

    fix app.py, added comments

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,26 +3,32 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from os import getenv  # to use environmental variables
 from flask import jsonify
-from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import HTTPException  # to use errorhandler
 
 
+# instance app variable from flask class
 app = Flask(__name__)
+# registers the blueprint app_views for usage
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown(self):
+    """removes the current sqlalchemy session"""
     storage.close()
 
 
 @app.errorhandler(HTTPException)
 def handle_exception(error):
+    """uses errorhandler to display 404 error page"""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
+    # return env variable if it exists
+    # otherwise return second argument
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True, debug=True)
