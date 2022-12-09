81fdbe39c981c0fcdb2945475b44630b427be5ad
Raulj06 <5015@holbertonschool.com>
2022-12-09 06:54:39 +0000

    task 4

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' module that have the get status '''
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status')
+def get_status():
+    ''' route status test that return a JSON query '''
+    dict = {'status': 'OK'}
+    return jsonify(dict)
