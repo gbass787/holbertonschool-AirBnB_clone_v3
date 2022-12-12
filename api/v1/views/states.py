182a456e0e27b90c2b24bc5bffb77ecd91ab6621
Raulj06 <5015@holbertonschool.com>
2022-12-12 01:38:39 +0000

    typo

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,55 +1,44 @@
 #!/usr/bin/python3
-""" retrieve an object into a valid JSON """
+''' Module that handles all default RESTFul API '''
 
 from flask import jsonify, abort, request, Response
 from models import storage
-from models.city import City
-from models.user import User
-from models.place import Place
+from models.state import State
 from api.v1.views import app_views
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'],
-                 strict_slashes=False)
-def get_places(city_id=None):
-    """ focus on all the place objects """
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404)
+@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
+def get_states():
+    """ focus on all the state object """
 
     if request.method == 'POST':
         data = request.get_json()
         if not data:
             return Response("Not a JSON", 400)
-        if 'user_id' not in data:
-            return Response("Missing user_id", 400)
         if 'name' not in data:
             return Response("Missing name", 400)
-        user = storage.get(User, data.get('user_id'))
-        if not user:
-            abort(404)
-        place = Place(city_id=city.id, user_id=user.id, name=data.get('name'))
-        place.save()
-        return jsonify(place.to_list()), 201
+        state = State(name=data.get('name'))
+        state.save()
+        return jsonify(state.to_dict()), 201
 
-    all_places = city.places
-    places = []
+    all_states = storage.all('State')
+    states = []
 
-    for place in all_places:
-        places.append(place.to_list())
-    return jsonify(places), 200
+    for state in all_states.values():
+        states.append(state.to_dict())
+    return jsonify(states)
 
 
-@app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'],
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-def get_place(place_id=None):
-    """ focus only in a single place object """
-    place = storage.get(Place, place_id)
-    if place is None:
+def get_state(state_id=None):
+    """ focus on a single state object """
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
 
     if request.method == 'DELETE':
-        storage.delete(place)
+        storage.delete(state)
         storage.save()
         return jsonify({}), 200
 
@@ -57,12 +46,10 @@ def get_place(place_id=None):
         data = request.get_json()
         if not data:
             return Response("Not a JSON", 400)
-        data['id'] = place.id
-        data['user_id'] = place.user_id
-        data['city_id'] = place.city_id
-        data['created_at'] = place.created_at
-        place.__init__(**data)
-        place.save()
-        return jsonify(place.to_list()), 200
+        data['id'] = state.id
+        data['created_at'] = state.created_at
+        state.__init__(**data)
+        state.save()
+        return jsonify(state.to_dict()), 200
 
-    return jsonify(place.to_list()), 200
+    return jsonify(state.to_dict())
